07290ddb1ce3dc7bb2f7c37a7859c4ddd1665f0a
Praneeth-Kallakuri <108530419+Praneeth-Kallakuri@users.noreply.github.com>
2023-06-25 17:12:59 +0530

    Create App.js

---
+++ Assignment-1(Sign-in)/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button, TextInput, View, TestInput, ToastAndroid } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("")
+
+  const handleInputUsername = (e) => {
+    setUsername(e)
+    console.log(username)
+  }
+
+  const handleInputPassword = (e) => {
+    setPassword(e)
+    console.log(password)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt}>Sign-In</Text>
+      <View style={styles.input}>
+        <TextInput onChangeText={e => handleInputUsername(e)} defaultValue={username}  placeholder="Enter Username" />
+        <TextInput onChangeText={e => handleInputPassword(e)} defaultValue={password}  placeholder="Enter Password" secureTextEntry />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a2b6e0',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  txt: {
+    height: 100,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    fontSize: 30,
+    color: '#000000',
+    //paddingRight: 300,
+    paddingBottom: 50,
+  },
+  input: {
+      height: 20,
+      fontSize: 20
+  }
+});
+
+
